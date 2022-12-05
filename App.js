cdc0095cf2ba88ad1d62b79b353f9d1f54d4504c
Yash-Ikhankar <96953168+Yash-Ikhankar@users.noreply.github.com>
2022-12-05 14:17:15 +0530

    Added Login Page

---
+++ App.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const connectDB = require("./config/db");
+// Load config
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+const app = express();
+
+// Logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Handlebars
+app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+// static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", require("./routes/index"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server runnning in ${process.env.PORT} mode on port ${PORT}`)
+);
